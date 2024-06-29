b6085099ae1f94356931f13fe1c10994f4374d9e
timmyzai <timmycdc@gmail.com>
2024-06-29 16:13:32 +0800

    added cooldown and dm user

--- src/discord_bot/bot/commands.py
+++ src/discord_bot/bot/commands.py
@@ -2,19 +2,48 @@ import discord
 from discord.ui import Button
 import random
 import string
+import time
 
 class VerifyButton(Button):
     def __init__(self):
         super().__init__(label="Verify", style=discord.ButtonStyle.primary)
         self.codes = {}
+        self.user_cool_down = {}  # Track when users last clicked the button
+        self.cool_down_duration = 0  # Cool down duration in seconds
 
     def generate_confirmation_code(self, length=6):
         letters_and_digits = string.ascii_letters + string.digits
         return ''.join(random.choice(letters_and_digits) for _ in range(length))
 
     async def callback(self, interaction: discord.Interaction):
+        user_id = interaction.user.id
+        current_time = time.time()
+
+        # Check if the user is on cool down
+        if user_id in self.user_cool_down:
+            last_click_time = self.user_cool_down[user_id]
+            if current_time - last_click_time < self.cool_down_duration:
+                # User is on cool down, inform them to wait
+                await interaction.response.send_message("Please wait 60 seconds before clicking verify again.", ephemeral=True)
+                return
+
+        # Generate a unique confirmation code
         code = None
         while code is None or code in self.codes:
             code = self.generate_confirmation_code()
         self.codes[code] = [interaction.user.name, interaction.user.global_name, interaction.id]
-        await interaction.response.send_message(f'Your confirmation code is: {code}', ephemeral=True)
+
+        # Update the user's last click time
+        self.user_cool_down[user_id] = current_time
+
+        # Define the message
+        verification_message = f'Send verification code to account ClosureM at location 1153:906.\n\nCode: {code}'
+
+        # Send verification message in the channel
+        await interaction.response.send_message(verification_message, ephemeral=True)
+                                                
+        # Send direct verification message to the user
+        if interaction.user.dm_channel is None:
+            await interaction.user.create_dm()
+        await interaction.user.dm_channel.send(verification_message)
+
--- src/discord_bot/services/lok_service.py
+++ src/discord_bot/services/lok_service.py
@@ -12,6 +12,7 @@ class LokService:
         self.codes2discord  = {}  # this map confirmation code to discord user
         self.codes2LOK  = {} # this map confirmation code to LOK user
 
+    #login api to get access token
     def login(self):
         payload = '{"authType":"email","email":"%s","password":"%s","deviceInfo":{"build":"global","OS":"Windows 10","country":"USA","language":"English","bundle":"","version":"1.1775.158.242","platform":"web","pushId":""}}' % (USER, PASSWORD) 
         encoded_payload = 'json=' + urllib.parse.quote(payload)
@@ -28,13 +29,14 @@ class LokService:
             logging.error("Token not found in the response: %s", response.json())
             raise ValueError("Token not found")
 
-    def fetch_emails(self):
+    #get personal email list
+    def get_personal_email_list(self):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
             'Content-Type': 'application/x-www-form-urlencoded',
             'X-Access-Token': self.accessToken,
         }
-        payload = '{"category":5}'
+        payload = '{"category":0}'
         encoded_payload = 'json=' + urllib.parse.quote(payload)
         response = self.session.post(MAIL_URL, headers=headers, data=encoded_payload)
         response.raise_for_status()
@@ -64,11 +66,12 @@ class LokService:
                 return
 
         # Fetch emails from the external service
-        mails = self.fetch_emails()
+        mails = self.get_personal_email_list()
         for mail in mails:
             uid = mail['_id']
             username = mail['from']['name']
             subject = mail['subject']['subject']
+            content = mail['content']
             # Map subject to LOK user if it exists in the Discord mapping but not in LOK mapping
             if subject in self.codes2discord and subject not in self.codes2LOK:
                 self.codes2LOK[subject] = [username, uid]

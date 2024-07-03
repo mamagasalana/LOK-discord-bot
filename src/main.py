d2419d7be62908453732e3d7c69e2967e7df5df6
blacksmitinv <gitlabacc@blacksmith-innovation.com>
2024-07-03 13:23:59 +0800

    commit missed out

--- src/main.py
+++ src/main.py
@@ -4,12 +4,11 @@ from discord_bot.bot import LOKBOT
 from config.config import CHANNEL_ID, TOKEN
 from config.config import DYNAMO_DB_NAME
 from config.logger import setup_logging
-from services.lok_service import LokService
 from db.repository.user_game_info_repository import UserGameInfoRepository
 
 def display_all_game_info():
     # Create an instance of the DynamoDBManager with the correct table name
-    user_personal_info_repo = UserPersonalInfoRepository(DYNAMO_DB_NAME)
+    user_personal_info_repo = UserGameInfoRepository(DYNAMO_DB_NAME)
     
     # Fetch all game info entries
     game_infos = user_personal_info_repo.get_all_users_game_info()
@@ -27,11 +26,12 @@ async def main():
     logging.info("Starting LOK Discord Bot")
 
     # Initialize and start the bot
-    # lok_bot = LOKBOT()
-    # lok_bot.discord_bot(TOKEN, CHANNEL_ID)
+    lok_bot = LOKBOT()
+    lok_bot.discord_bot(TOKEN, CHANNEL_ID)
 
     # Run this to test the DynamoDBManager
-    display_all_game_info()
+    # Uncomment the line below to test the DynamoDBManager
+    # display_all_game_info()
 
 if __name__ == "__main__":
     asyncio.run(main())

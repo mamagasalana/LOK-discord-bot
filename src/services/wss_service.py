8fd4bb63f2f477b17edc3f6475555dc5b581be2d
ytee <ytee@butterfly.com.hk>
2024-12-21 11:21:01 +0800

    websocket header

--- src/services/wss_service.py
+++ src/services/wss_service.py
@@ -14,6 +14,18 @@ from config.logger import setup_logger
 class WSClosedException(Exception):
     pass
 
+WSS_HEADER = {
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    "Cache-Control": "no-cache",
+    "Connection": "Upgrade",
+    "Host": "socf-lok-live.leagueofkingdoms.com",
+    "Origin": "https://play.leagueofkingdoms.com",
+    "Pragma": "no-cache",
+    "Upgrade": "websocket",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+}
+
 class LOKWSS:
     def __init__(self, service:crypto, world=26, logger_name='wss'):
         self.service = service
@@ -122,7 +134,7 @@ class LOKWSS:
             self.last_zone = []
 
             try:
-                async with self.session.ws_connect(self.url, heartbeat=1.0, autoping=True) as ws:
+                async with self.session.ws_connect(self.url, headers=WSS_HEADER, heartbeat=1.0, autoping=True) as ws:
                     SUCCESS = await self.listen(ws)
             except WSClosedException:
                 logging.warning("WS closed, retrying in 5 seconds")
